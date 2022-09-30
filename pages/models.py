4425a4e078d9b96ef03299e9f3928ae35a65b41d
hannad <Hnadi Sindi@WS-302575-LT11.ksa.hsagroup.com>
2022-09-30 22:30:45 +0300

    testing

---
+++ pages/models.py
@@ -0,0 +1,30 @@
+from email.mime import image
+from django.db import models
+#from django.contrib.auth.models import User
+
+
+
+
+#class Employee(models.Model):
+  # user = models.OneToOneField(User, on_delete=models.CASCADE)
+  # phonenumber = models.CharField(verbose_name="phone number", max_lenght=10)
+  # birthdate = models.DateField(verbose_name="birth date")
+
+class pages(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+  
+
+
+class Product(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    Dept_name = models.CharField(max_length=20)
+    joining_date = models.DateField()
+    Price = models.IntegerField()
+    image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    
+
+    
+
+
